456abdf1bb951e2d21b2d23c2820aed88a407733
Pcfic <pcfic@Pcfics-MacBook-Pro.local>
2023-11-02 18:58:51 +0100

    first change

---
+++ GameCharacter.java
@@ -0,0 +1,28 @@
+public abstract class GameCharacter {
+
+    private String name;
+    private int health;
+    private int level;
+
+    public GameCharacter(String name, int health, int level) {
+        this.name = name;
+        this.health = health;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public abstract void attack();
+    public abstract void defend();
+    public abstract void printCharacterDetails();
+}
