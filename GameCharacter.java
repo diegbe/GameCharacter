30cd3ab77591b00c9acb6cffeb570a7b580ceb23
Pcfic <pcfic@Pcfics-MacBook-Pro.local>
2023-11-02 19:16:09 +0100

    add class

--- GameCharacter.java
+++ GameCharacter.java
@@ -24,5 +24,6 @@ public abstract class GameCharacter {
 
     public abstract void attack();
     public abstract void defend();
+    public abstract void teletransport();
     public abstract void printCharacterDetails();
 }
